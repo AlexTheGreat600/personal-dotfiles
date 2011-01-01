5b810ff133a9adec34d6bc253e1a1d03f1c535c1
AlexTheGreat600 <shivanshsrivastava@gmail.com>
2011-01-01 05:41:53 +0530

    added: fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,6 +2,8 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+source ~/.bash-aliases
+
 # Remove the greeting message
 set -g fish_greeting ""
 
@@ -12,6 +14,11 @@ fish_config prompt choose scales
 #set fish_prompts (fish_config prompt list)
 #set fish_random_prompt $fish_prompts[(math (random 1 (count $fish_prompts)))]
 #fish_config prompt choose $fish_random_prompt
+
+# Set the random theme of the shell
+#set fish_themes (fish_config theme list)
+#set fish_random_theme $fish_themes[(math (random 1 (count $fish_themes)))]
+#fish_config theme choose $fish_random_theme
 
 # Show the random colorscript
 #echo && colorscript -r && echo
